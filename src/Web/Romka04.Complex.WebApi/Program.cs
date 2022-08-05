74d56916069c02fad4b8301cfc74d8197380cd8e
romka-04 <chernobrovkin.roman@gmail.com>
2022-08-05 19:16:44 +0300

    Finish web-api project.

--- src/Web/Romka04.Complex.WebApi/Program.cs
+++ src/Web/Romka04.Complex.WebApi/Program.cs
@@ -5,6 +5,8 @@ using MiniValidation;
 using Romka04.Complex.Models;
 using Romka04.Complex.WebApi;
 using Romka04.Complex.WebApi.Database;
+using Romka04.Complex.WebApi.Database.Entities;
+using StackExchange.Redis;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,7 +16,7 @@ builder.Services.AddDbContext<DatabaseContext>((provider, options) =>
     var connStr = provider.GetService<IConfiguration>().GetConnectionString("pgDatabase");
     options.UseNpgsql(connStr);
 });
-builder.Services.AddOptions<RedisOptions>(RedisOptions.Name);
+builder.Services.AddOptions<RedisOptions>().Bind(builder.Configuration.GetSection(RedisOptions.Name));
 builder.Services.AddEndpointsApiExplorer();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddSwaggerGen();
@@ -40,22 +42,19 @@ app.MapGet("/values/all", async (HttpContext context) =>
 
 app.MapGet("/values/current", async (HttpContext context) =>
 {
-    var redisOptions = context.RequestServices.GetService<IOptions<RedisOptions>>()?.Value;
-    await Task.Delay(1);
-    // request redis for values that were used during current session
-    var result = new FabPair[]
-    {
-        new(0, 0),
-        new(1, 1),
-        new(2, 1),
-        new(3, 2),
-        new(4, 3),
-        new(5, 5),
-        new(6, 8),
-        new(19, 4181),
-    };
+    var redisOptions = GetRedisOptions<RedisOptions>(context);
 
-    return result;
+    using ConnectionMultiplexer redis = ConnectionMultiplexer
+        .Connect(redisOptions.GetConfiguration());
+    var db = redis.GetDatabase();
+    
+    var res = new List<FabPair>();
+    await foreach (var dto in db.HashScanAsync("values"))
+    {
+        res.Add(FabPair.Parse(dto.Name, dto.Value));
+    }
+
+    return Results.Ok(res.ToArray());
 });
 
 app.MapPost("/values", async (FabRequest request, HttpContext context) =>
@@ -63,14 +62,55 @@ app.MapPost("/values", async (FabRequest request, HttpContext context) =>
     if (!MiniValidator.TryValidate(request, out var errors))
         return Results.BadRequest(errors);
 
-    // calc new value and add it to database and redis
-    // ...
-    await Task.Delay(1);
+    // calc new value
     var index = request.Index.Value;
     var value = FabCalculator.Fab(index);
     var response = new FabResponse(index, value);
+
+    // add it to database and redis
+    var redisOption = GetRedisOptions<RedisOptions>(context);
+    var t1 = SetRedis(index, value, redisOption);
+
+    var t2 = SetDatabase(index, value, context);
+    
+    await TaskExt.WhenAll(t1, t2);
 
     return Results.Ok(response);
 });
 
 app.Run();
+
+async Task SetRedis(int index, int value, RedisOptions redisOptions)
+{
+    using ConnectionMultiplexer redis = ConnectionMultiplexer
+        .Connect(redisOptions.GetConfiguration());
+    var db = redis.GetDatabase();
+
+    await db.HashSetAsync("values", new HashEntry[] { new(index, value) });
+}
+
+async Task SetDatabase(int index, int value, HttpContext context, CancellationToken cancellationToken = default)
+{
+    await using var db = context.RequestServices.GetService<DatabaseContext>();
+    var newEntity = new ValuesEntity { Number = index };
+    var exists = await db.Values.AnyAsync(x => x.Number == newEntity.Number);
+    if (!exists)
+    {
+        await db.Values.AddAsync(newEntity);
+        await db.SaveChangesAsync();
+    }
+}
+
+TOptions GetRedisOptions<TOptions>(HttpContext context)
+    where TOptions : class
+{
+    if (null == context) throw new ArgumentNullException(nameof(context));
+
+    var options = context.RequestServices.GetService<IOptions<TOptions>>();
+    if (null == options) throw new Exception("Please register options during service registration");
+
+    return options.Value;
+}
+
+
+
